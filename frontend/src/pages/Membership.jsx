307bb14feb3f668ea0fad38e793bc8ac4aff1a1c
Arpit Bisht <161747201+ArpitBisht2302@users.noreply.github.com>
2026-02-04 08:08:53 +0530

    customize for phone

--- frontend/src/pages/Membership.jsx
+++ frontend/src/pages/Membership.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-
-// import "./Membership.css";
-
-// import { useNavigate } from "react-router-dom";
-
-// export default function Membership() {
-//   const navigate = useNavigate();
-
-//   return (
-//     <section
-//       className="membership-section"
-//       style={{ height: "600px", position: "relative" }}
-//     >
-//       <div className="membership-left">
-//         <h1>Choose Your Perfect Membership</h1>
-//         <p>
-//           Train solo or with your partner. Flexible plans, premium equipment,
-//           and an environment built for real transformation.
-//         </p>
-
-//         <button
-//           className="join-btn"
-//           style={{ backgroundColor: "white", color: "black" }}
-//           onClick={() => navigate("/plans")}
-//         >
-//           Join Now
-//         </button>
-//       </div>
-//     </section>
-//   );
-// }
-
 import React from "react";
 import "./Membership.css";
 import { useNavigate } from "react-router-dom";
