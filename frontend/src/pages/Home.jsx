a8abc0ae311c7cf63dd734a6951a7d1480cdd4f9
Arpit Bisht <161747201+ArpitBisht2302@users.noreply.github.com>
2026-02-05 15:43:47 +0530

    dashboard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import ScrollVelocity from "../components/ScrollVelocity";
 import "../components/ScrollVelocity.css";
 
 import Membership from "./Membership";
+import CardSwap, { Card } from "../components/CardSwap";
 
 import EnquiryCTA from "../components/EnquiryCTA";
 
@@ -117,6 +118,27 @@ function Home() {
       />
 
       <EnquiryCTA />
+      <div style={{ height: "600px", position: "relative" }}>
+        <CardSwap
+          cardDistance={60}
+          verticalDistance={70}
+          delay={5000}
+          pauseOnHover={false}
+        >
+          <Card>
+            <h3>Card 1</h3>
+            <p>Your content here</p>
+          </Card>
+          <Card>
+            <h3>Card 2</h3>
+            <p>Your content here</p>
+          </Card>
+          <Card>
+            <h3>Card 3</h3>
+            <p>Your content here</p>
+          </Card>
+        </CardSwap>
+      </div>
 
       {showChoice && (
         <AuthChoiceModal
